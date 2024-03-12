159d7a05d35aa5ded09861493fb2fad6683af9ef
Abrham <abrsh6266@gmail.com>
2024-03-12 10:08:13 -0800

    final update

--- src/Submenu.jsx
+++ src/Submenu.jsx
@@ -1,11 +1,28 @@
+import { useRef } from "react";
 import { useGlobalContext } from "./Context";
 import sublinks from "./data";
 
 const Submenu = () => {
-  const { pageId } = useGlobalContext();
+  const { pageId, setPageId } = useGlobalContext();
   const currentPage = sublinks.find((link) => link.pageId === pageId);
+
+  const submenuContainer = useRef(null);
+  const handleMouseLeave = (event) => {
+    //setPageId(null);
+    const submenu = submenuContainer.current;
+    const {left,right,bottom} = submenu.getBoundingClientRect();
+    const { clientX, clientY } = event;
+    if (clientX < left - 1 || clientX>right-1 || clientY >bottom-1) {
+      setPageId(null);
+    }
+    console.log(clientX, clientY);
+  };
   return (
-    <div className={currentPage ? "submenu show-submenu" : "submenu"}>
+    <div
+      ref={submenuContainer}
+      onMouseLeave={handleMouseLeave}
+      className={currentPage ? "submenu show-submenu" : "submenu"}
+    >
       <h5>{currentPage?.page}</h5>
       <div
         style={{
